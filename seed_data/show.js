c219598e1ebc40a50a496b93f0f06f14a67fc498
Jack <jackstasiulis@gmail.com>
2022-12-09 14:50:37 -0800

    update seed data with new info

--- seed_data/show.js
+++ seed_data/show.js
@@ -2,7 +2,7 @@ module.exports =
 [
     {
         show_id:1,
-        image: 'bop',
+        image: 'https://res.cloudinary.com/do5k651qd/image/upload/v1670570548/Screen_Shot_2022-11-16_at_10.41.43_PM_tvoox9.png',
         artist:'Seeing Strangers',
         date:'Feb, 23, 2023',
         venue:'The Biltmore Cabaret',
@@ -14,7 +14,7 @@ module.exports =
     },
     {
         show_id:2,
-        image: 'bop',
+        image: 'https://res.cloudinary.com/do5k651qd/image/upload/v1670570334/Screenshot_2022-12-08_at_11.02.16_PM_azlqpp.jpg',
         artist:'FAVIO',
         date:'Mar 24 2023',
         venue:'The Roxy',
@@ -26,7 +26,7 @@ module.exports =
     },
     {
         show_id:3,
-        image: 'bop',
+        image: 'https://res.cloudinary.com/do5k651qd/image/upload/v1670570334/Screenshot_2022-12-08_at_11.05.11_PM_ihukpb.jpg',
         artist:'BAD MONEY',
         date:'Jan, 17, 2023',
         venue:'The Fox Cabaret',
@@ -38,15 +38,63 @@ module.exports =
     },
     {
         show_id:4,
-        image: 'bop',
+        image: 'https://res.cloudinary.com/do5k651qd/image/upload/c_crop,g_custom,h_1852,w_2796,x_0,y_-300/v1670570335/IMG_3535_fofovb.jpg',
         artist:'killr kate',
         date:'Dec, 4, 2022',
         venue:'Toys R Us Rooftop',
         address:'1154 W Broadway',
         doors:'8:00 PM',
         genre:'Indie Pop',
-        description: 'lets cry'
+        description: 'Come see killr kate woo you with her unique and displaced voice. Wear your best skeleton mask to fit in.'
         // timestamp: '1628522461000'
-    }
+    },
+    {
+        show_id:5,
+        image: 'https://res.cloudinary.com/do5k651qd/image/upload/v1670570334/IMG_0764_ad0k30.jpg',
+        artist:'frong',
+        date:'Jan, 20, 2023',
+        venue: "Gracie's Basement",
+        address:'TBA',
+        doors:'10:00 PM',
+        genre:'Grunge',
+        description: 'If you adore the mosh and throwing down a little too hard than your doctor wishes, come put your bodily health in jeopardy in our dirty basement.'
+        // timestamp: '1628522461000'
+    },
+    {
+        show_id:6,
+        image: 'link',
+        artist:'BADMONEY',
+        date:'Jan2023',
+        venue:'TheCabaret',
+        address:'232St',
+        doors:'8:0PM',
+        genre:'Rock',
+        description: 'letock'
+        // timestamp: '1628522461000'
+    },
+    {
+        show_id:7,
+        image: 'link',
+        artist:'BADMONEY',
+        date:'Jan2023',
+        venue:'TheCabaret',
+        address:'232St',
+        doors:'8:0PM',
+        genre:'Rock',
+        description: 'letock'
+        // timestamp: '1628522461000'
+    },
+    {
+        show_id:8,
+        image: 'link',
+        artist:'BADMONEY',
+        date:'Jan2023',
+        venue:'TheCabaret',
+        address:'232St',
+        doors:'8:0PM',
+        genre:'Rock',
+        description: 'letock'
+        // timestamp: '1628522461000'
+    },
 ]
 
