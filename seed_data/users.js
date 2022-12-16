0a69f4adb8ee8e572d73972b0a356bc81b0c2d92
Jack <jackstasiulis@gmail.com>
2022-12-16 11:49:05 -0800

    edit users array

--- seed_data/users.js
+++ seed_data/users.js
@@ -3,9 +3,16 @@ module.exports =
 [
     {
       users_id: 1,
-      username: 'jackstas',
       email: 'jackstasiulis@gmail.com',
+      username: 'jackstas',
       password: 'backyard23',
       avatar: 'X'
+    },
+    {
+      users_id: 2,
+      email: 'bob2@gmail.com',
+      username: 'bob2',
+      password: 'bobpass2',
+      avatar: 'XX'
     }
   ]
